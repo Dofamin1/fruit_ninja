9f55f221ab1b9e4bf4e853558c54d1cca11bc1dc
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-01-24 09:41:36 -0800

    створив шаблон "архітектури" гри

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("hello World!")
+//TODO: package.json
+require("phaser");
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
