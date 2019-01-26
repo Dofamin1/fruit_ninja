6a7363e7c49695ca8f7bc45cbea156d3e6be9f5b
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-01-26 02:13:56 -0800

    підключив Phaser через html + переробив код під нову версію Phaser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 //TODO: package.json
-// require("./phaser.js");
-require("phaser");
 const data = [
   { name: "pineapple", points: 3 },
   { name: "watermelon", points: 5 },
@@ -13,86 +11,89 @@ const centerY = window.innerHeight / 2; //TODO: подумати над цим
 const centerX = window.innerWidth / 2;
 const fruitSize = [200, 200];
 let score = 0;
+let scoreText;
 
-const config = {
-  type: Phaser.AUTO,
-  width: innerWidth,
-  height: innerHeight,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 500 },
-      debug: false
-    }
-  },
-  scene: {
+var game = new Phaser.Game(
+  innerWidth,
+  innerHeight,
+  Phaser.AUTO,
+  "fruit-ninja",
+  {
     preload: preload,
     create: create,
     update: update
   }
+);
+
+const createScore = () => {
+  scoreText = game.add.text(16, 16, "Score: 0", {
+    fontSize: "32px",
+    fill: "#fff"
+  });
 };
-
-const game = new Phaser.Game(config);
-
-function createFruitGenerator(scoreText) {
+const createFruitGenerator = () => {
   const addToScore = ({ points }) => {
     score = score + points * 10;
     scoreText.setText("Score: " + score);
   };
   const setEventListeners = fruit => {
-    fruit.on("pointerup", () => {
-      addToScore(fruit.data.values);
+    fruit.events.onInputDown.add(() => {
+      addToScore(fruit.data);
       fruit.destroy();
     });
+    fruit.events.onOutOfBounds.add(() => {});
   };
   const setVelocity = (startPointX, fruit) => {
-    const velocityY = Phaser.Math.Between(-700, -1200);
+    const velocityY = game.math.between(-400, -800);
     const velocityX =
       startPointX > innerWidth / 2
-        ? Phaser.Math.Between(-90, -170)
-        : Phaser.Math.Between(90, 170);
-    fruit.setVelocityY(velocityY);
-    fruit.setVelocityX(velocityX);
+        ? game.math.between(-90, -170)
+        : game.math.between(90, 170);
+    fruit.body.velocity.setTo(velocityX, velocityY);
   };
-  const getRandomFruit = () => data[Phaser.Math.Between(0, 3)];
-  const getRandomX = () => Phaser.Math.Between(0, innerWidth);
+  const getRandomFruit = () => data[game.math.between(0, 3)];
+  const getRandomX = () => game.math.between(0, innerWidth);
   const createFruit = () => {
     const x = getRandomX();
     const y = innerHeight;
     const { name, points } = getRandomFruit();
-    const fruit = this.physics.add.sprite(x, y, name);
-    fruit
-      .setSize(...fruitSize)
-      .setDisplaySize(...fruitSize)
-      .setData({ points })
-      .setInteractive();
+    const fruit = game.add.sprite(x, y, name);
+    game.physics.enable(fruit, Phaser.Physics.ARCADE);
+    fruit.body.gravity.y = 300;
+    fruit.inputEnabled = true;
+    fruit.checkWorldBounds = true;
+    fruit.width = 200;
+    fruit.height = 200;
+    fruit.data.points = points;
     setEventListeners(fruit);
     setVelocity(x, fruit);
   };
   setInterval(createFruit, 800);
-}
+};
+const createLives = () => {
+  const lives = game.add.group();
+  const livesCount = 3;
+  for (var i = 0; i < livesCount; i++) {
+    const ship = lives.create(game.world.width + 90 * i, 60, "heart");
+    ship.anchor.setTo(2.5, 0.5);
+  }
+};
 
 function preload() {
-  this.load.image("background", "./images/background.png");
-  this.load.image("pineapple", "./images/pineapple.png");
-  this.load.image("watermelon", "./images/watermelon.png");
-  this.load.image("banana", "./images/banana.png");
-  this.load.image("apple", "./images/apple.png");
-  this.load.image("heart", "./images/heart.png");
+  game.load.image("background", "./images/background.png");
+  game.load.image("pineapple", "./images/pineapple.png");
+  game.load.image("watermelon", "./images/watermelon.png");
+  game.load.image("banana", "./images/banana.png");
+  game.load.image("apple", "./images/apple.png");
+  game.load.image("heart", "./images/heart.png");
 }
 
 function create() {
-  this.add.tileSprite(centerX, centerY, game.width, game.height, "background");
-  const scoreText = this.add.text(16, 16, "Score: 0", {
-    fontSize: "32px",
-    fill: "#fff"
-  });
-  const lives = this.physics.add.staticGroup({
-    key: "heart",
-    repeat: 2,
-    setXY: { x: innerWidth - 280, y: 50, stepX: 90 }
-  });
-  createFruitGenerator.call(this, scoreText);
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.add.tileSprite(0, 0, game.width, game.height, "background");
+  createScore();
+  createLives();
+  createFruitGenerator();
 }
 
 function update() {}
