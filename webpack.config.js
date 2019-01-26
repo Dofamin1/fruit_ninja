f7f2396fd214ef1bd0941d823a2bbcd15806a463
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-01-26 02:46:27 -0800

    форматнув код

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,59 @@
-const path = require('path');
-const argv = require('yargs').argv;
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const argv = require("yargs").argv;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const isDevelopment = argv.mode === 'development';
-const distPath = path.join(__dirname, '/public');
+const isDevelopment = argv.mode === "development";
+const distPath = path.join(__dirname, "/public");
 
 const config = {
   entry: {
-    main: './src/index.js'
+    main: "./src/js/index.js"
   },
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: distPath
   },
   module: {
-    rules: [ 
-    {
+    rules: [
+      {
         test: /\.html$/,
-        use: 'html-loader'
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader'
-      }]
-    }, {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      use: [
-        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader
-      ]
-     }, 
+        use: "html-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader]
+      }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'index.css',
+      filename: "index.css"
     }),
     new HtmlWebpackPlugin({
-        template: './src/index.html'
+      template: "./src/index.html"
     }),
-    new CopyWebpackPlugin([{ 
-      from: 'src/assets', to: 'images' 
-    }])
+    new CopyWebpackPlugin([
+      {
+        from: "src/assets",
+        to: "images"
+      }
+    ])
   ],
   devServer: {
     contentBase: distPath,
-    port: 8000,
+    port: 8000
   }
 };
 
